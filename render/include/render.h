fc92a3d4691c816c50805be12e19a0293e82ca24
MikeShaw1 <1481796592@qq.com>
2022-04-03 18:42:53 +0800

    init

---
+++ render/include/render.h
@@ -0,0 +1,17 @@
+#ifndef RENDER_H
+#define RENDER_H
+
+#include <iostream>
+#include <stdio.h>
+#include <glad/glad.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <imgui/imgui.h>
+#include <backends/imgui_impl_sdl.h>
+#include <backends/imgui_impl_opengl3.h>
+#include <stb/stb_image.h>
+
+
+
+
+#endif // !RENDER_H
